e6c44730b79b0b4bf35304d377ac604e261384d9
zq <929679517@qq.com>
2022-05-05 15:51:16 +0800

    update

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -193,7 +193,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
   // const isRootPage =
   //   parsePageId(block?.id) === parsePageId(site?.rootNotionPageId)
   const isBlogPost =
-    block?.type === 'page' && block?.parent_table === 'collection'
+    block?.type === 'page' && (block?.parent_table === 'block' || block?.parent_table === 'collection')
 
   const showTableOfContents = !!isBlogPost
   const minTableOfContentsItems = 3
